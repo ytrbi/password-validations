390d747a34d75a41ee0ddb592a915b278c6fa11a
Yaxi <yaturbi43@gmail.com>
2024-05-22 04:48:18 +0300

    second commit

--- script.js
+++ script.js
@@ -59,3 +59,18 @@ function check() {
     }
 
 }
+
+function myFunction() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
